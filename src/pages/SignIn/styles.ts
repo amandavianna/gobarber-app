f9589eb2bfc49550581cd5b9432966931f2a7386
Amanda Vianna <amanda_vianna@hotmail.com>
2020-07-23 13:18:20 -0300

    Add registration screen

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
 `;
 
 export const Title = styled.Text`
@@ -33,7 +34,7 @@ export const CreateAccountButton = styled.TouchableOpacity`
   background: #312e38;
   border-top-width: 1px;
   border-top-color: #232129;
-  padding: 16px 0 ${getBottomSpace()}px;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
 
   flex-direction: row;
   justify-content: center;
